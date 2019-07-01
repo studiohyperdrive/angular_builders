3452348905e3e5f3d101f865d4688a8c7b03f2f3
Tom Opdebeeck <tom.opdebeeck@district01.be>
2019-07-01 14:36:21 +0200

    fix: remove build files from git

--- lib/generate-schemas/generate-schema.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: (filePath: string, dir: string) => Promise<unknown>;
-export default _default;
--- lib/utils/clean-dir.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: (target: string) => Promise<unknown>;
-export default _default;
