a493cc844932964813e899d8ccc8ca94e5b41112
Tom Opdebeeck <tom.opdebeeck@studiohyperdrive.be>
2021-06-07 17:05:51 +0200

    fix: add peerdeps to dist

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -16,6 +16,7 @@ const {
 	bugs,
 	homepage,
 	dependencies,
+	peerDependencies,
 } = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'package.json'), { encoding: 'UTF-8' }));
 
 fs.writeFileSync(path.resolve(process.cwd(), 'dist', 'package.json'), JSON.stringify({
@@ -32,6 +33,7 @@ fs.writeFileSync(path.resolve(process.cwd(), 'dist', 'package.json'), JSON.strin
 	bugs,
 	homepage,
 	dependencies,
+	peerDependencies,
 }, null, 2), { encoding: 'UTF-8' });
 
 cpx.copySync(path.resolve(process.cwd(), 'builders.json'), path.resolve(process.cwd(), 'dist'));
