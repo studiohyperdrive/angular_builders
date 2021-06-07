41d695eaa2bdb0a61bc5d90192ed169500b78638
Tom Opdebeeck <tom.opdebeeck@studiohyperdrive.be>
2021-06-07 14:56:59 +0200

    fix: cleanup build * switch to yarn * build to dist folder * copy package.json, readme & license to dist

---
+++ scripts/prepublish.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+const cpx = require('cpx');
+
+const {
+	name,
+	version,
+	description,
+	files,
+	main,
+	types,
+	builders,
+	repository,
+	author,
+	license,
+	bugs,
+	homepage,
+	dependencies,
+} = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), 'package.json'), { encoding: 'UTF-8' }));
+
+fs.writeFileSync(path.resolve(process.cwd(), 'dist', 'package.json'), JSON.stringify({
+	name,
+	version,
+	description,
+	files,
+	main,
+	types,
+	builders,
+	repository,
+	author,
+	license,
+	bugs,
+	homepage,
+	dependencies,
+}, null, 2), { encoding: 'UTF-8' });
+
+cpx.copySync(path.resolve(process.cwd(), 'builders.md'), path.resolve(process.cwd(), 'dist'));
+cpx.copySync(path.resolve(process.cwd(), 'LICENSE.md'), path.resolve(process.cwd(), 'dist'));
+cpx.copySync(path.resolve(process.cwd(), 'README.md'), path.resolve(process.cwd(), 'dist'));
